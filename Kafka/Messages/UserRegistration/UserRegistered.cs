8f043f2f9faf3f0a0403d4e768634a60de780648
amit tyagi <amit_tyagi@external.mckinsey.com>
2021-04-01 01:54:44 +0530

    Moved code out of src folder

---
+++ Kafka/Messages/UserRegistration/UserRegistered.cs
@@ -0,0 +1,13 @@
+﻿namespace Kafka.Messages.UserRegistration
+{
+	/// <summary>
+	///  Represents the message object in UserRegistered Topic
+	/// </summary>
+	public class UserRegistered
+	{
+		/// <summary>
+		/// Indicates UserId of the registered User
+		/// </summary>
+		public int UserId { get; set; }
+	}
+}
